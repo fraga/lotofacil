e028e402697c8267d879d90caca09a3c6fc263a0
fraga <rodrigo.fraga@gmail.com>
2010-12-09 10:39:26 -0200

    - My first version 3.0.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lotofacil::Application.config.secret_token = 'b8bfd3338bc7066d1f3ffd6a338f72bf8cb51addb724625d1d3bba3569bac029403ddd73f8276ab8de2426996cc8a312062f6973b4eb82cb38d95405de245308'
