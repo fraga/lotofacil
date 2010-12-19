c247e00847b8b7e55b0bd7197a3438544f9faa6d
fraga <rodrigo.fraga@gmail.com>
2010-12-19 12:47:49 -0200

    - Adding details to lotofacil table structure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101209164513) do
+ActiveRecord::Schema.define(:version => 20101211030813) do
 
   create_table "loto_facil_games", :force => true do |t|
     t.string   "n1"
@@ -31,6 +31,21 @@ ActiveRecord::Schema.define(:version => 20101209164513) do
     t.text     "hash"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "game_id"
+    t.datetime "game_date"
+    t.integer  "winners_15"
+    t.integer  "winners_14"
+    t.integer  "winners_13"
+    t.integer  "winners_12"
+    t.decimal  "amount_div_15"
+    t.decimal  "amount_div_14"
+    t.decimal  "amount_div_13"
+    t.decimal  "amount_div_12"
+    t.decimal  "amount_div_11"
+    t.decimal  "amount_accumulated_15"
+    t.decimal  "amount_prize_estimate"
   end
+
+  add_index "loto_facil_games", ["game_id"], :name => "index_loto_facil_games_on_game_id"
 
 end
